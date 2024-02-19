dee19199241b9d8fb3af2a92a7d5f7618edccb94
Nesrux <joaomarcosdevs@gmail.com>
2024-02-19 12:32:15 -0300

    feat: adiciona listagem de categorias

--- internal/services/category.go
+++ internal/services/category.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/nesrux/grpc/internal/database"
 	"github.com/nesrux/grpc/internal/pb"
- )
+)
 
 type CategoryService struct {
 	pb.UnimplementedCategoryServiceServer
@@ -30,4 +30,24 @@ func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCateg
 	}
 
 	return categoryResponse, nil
+}
+func (c *CategoryService) ListCategories(ctx context.Context, in *pb.Blank) (*pb.CategoryList, error) {
+	categories, err := c.CategoryDb.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var categoriesResponse []*pb.Category
+
+	for _, category := range categories {
+		categoryResponse := &pb.Category{
+			Id:          category.ID,
+			Name:        category.Name,
+			Description: category.Description,
+		}
+
+		categoriesResponse = append(categoriesResponse, categoryResponse)
+	}
+
+	return &pb.CategoryList{Categories: categoriesResponse}, nil
 }
